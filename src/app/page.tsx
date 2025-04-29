87df18e428ec2ac51add252e31fe8aada91fc9be
Ana Paula Garcia Martins <martinsgarciaana@outlook.com>
2025-04-29 11:51:44 -0300

    Feat: Adicionado efeito hover e cursor no botão explore comigo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       </main>
       <footer className='flex p-4 md:justify-center'>
         <Link href='/sobre-mim'>
-          <button className='flex items-center justify-center gap-2 px-4 py-3 bg-white text-black font-medium rounded-full '>
+          <button className='flex items-center justify-center gap-2 px-4 py-3 bg-white text-black font-medium rounded-full cursor-pointer hover:bg-pink transition-colors'>
             Explore comigo
             <ArrowRight></ArrowRight>
           </button>
